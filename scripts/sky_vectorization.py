99570e576e4b841ed4147f342c9a472c11b628f8
Joe Harding <joe.harding@us.ibm.com>
2017-06-15 14:32:37 -0500

    further iteration on the sky vectorization

--- scripts/sky_vectorization.py
+++ scripts/sky_vectorization.py
@@ -43,26 +43,38 @@ def get_pixel_below(grid_width, grid_height, idx):
         return ij_to_idx(grid_height, i, j + 1)
 
 
+def average_color(average, color):
+    diff = (average[0] - color[0], average[1] - color[1], average[2] - color[2])
+    #pdb.gimp_message("color diff: {d}".format(d=diff))
+    return (average[0] + diff[0], average[1] + diff[1], average[2] + diff[2])
+
+
+def is_color_close(average, color):
+    diff = (average[0] - color[0]) + (average[1] - color[1]) + (average[2] - color[2])
+    #pdb.gimp_message("color diff: {d}".format(d=diff))
+    return diff < 5
+
+
 # returns an array of pixel indices that belong to the current section
-def scan_pixels(current_index, pixels, pixel_sectioned):
+def scan_pixels(current_index, pixels, pixel_sectioned, color_average, width, height):
+    #pdb.gimp_message("before current index: {i}, width: {w}, height: {h}".format(i=current_index, w=width, h=height)) 
     result = []
-    #pdb.gimp_message("start: {i}".format(i=current_index)) 
     if current_index is not None and current_index not in pixel_sectioned:
-        #pdb.gimp_message("not none: {i}".format(i=current_index)) 
-        pixel_sectioned[current_index] = True
-        if pixels[current_index][0] == 255:
-            result.append(current_index) 
-            #pdb.gimp_message("appended: {i}".format(i=current_index)) 
+        if is_color_close(color_average, pixels[current_index]):
+            color_average = average_color(color_average, pixels[current_index]) 
             
-            result += scan_pixels(get_pixel_right(20, 20, current_index), pixels, pixel_sectioned)
-            result += scan_pixels(get_pixel_left(20, 20, current_index), pixels, pixel_sectioned)
-            result += scan_pixels(get_pixel_above(20, 20, current_index), pixels, pixel_sectioned)
-            result += scan_pixels(get_pixel_below(20, 20, current_index), pixels, pixel_sectioned)
+            result.append(current_index) 
+            pixel_sectioned[current_index] = True 
+            
+            result += scan_pixels(get_pixel_right(width, height, current_index), pixels, pixel_sectioned, color_average, width, height)
+            result += scan_pixels(get_pixel_left(width, height, current_index), pixels, pixel_sectioned, color_average, width, height)
+            result += scan_pixels(get_pixel_above(width, height, current_index), pixels, pixel_sectioned, color_average, width, height)
+            result += scan_pixels(get_pixel_below(width, height, current_index), pixels, pixel_sectioned, color_average, width, height)
 
     return result
 
 
-def make_sections(pixels):
+def make_sections(pixels, width, height):
     # for each pixel if it's not already a member of a section this is the first pixel of the section
     # so create a new entry in the sections array, pass that section index into the scan_pixels in the upcoming method calls
     # feed that pixel into scan_pixels, a recursive function that checks the pixel's section membership
@@ -71,63 +83,67 @@ def make_sections(pixels):
      
     pixel_sectioned = {} # if the index is fill with a True that means it already belongs to a section
     sections = [] # array of arrays containing indices of pixels that belong to each section
-    for idx in range(len(pixels)):
-        sections.append(scan_pixels(idx, pixels, pixel_sectioned))
- 
+    for idx, pixel in enumerate(pixels):
+        new_section = scan_pixels(idx, pixels, pixel_sectioned, pixel, width, height) 
+        if len(new_section) > 0:
+            sections.append(new_section)
+
     return sections
 
 
-def do_work():
-    img = gimp.Image(20, 20, RGB)
-   
-    # image for testing
-    background = gimp.Layer(img, "Background", img.width, img.height, RGB_IMAGE, 100, NORMAL_MODE)
-    background.fill(BACKGROUND_FILL)
-  
+def create_pixels(img):
+    pdb.gimp_context_set_foreground((255, 255, 255))
+    
+    background = gimp.Layer(img, "seed", img.width, img.height, RGBA_IMAGE, 100, NORMAL_MODE)
+    background.fill(TRANSPARENT_FILL) 
+    
     img.add_layer(background, 1)
 
-    #pdb.gimp_context_push()
+    pdb.gimp_context_push()
 
     actual_name = pdb.gimp_brush_new('joe')
     pdb.gimp_message("brush name: {a}".format(a=actual_name)) 
-    
-    actual_hardness = pdb.gimp_brush_set_hardness(actual_name, 1.0)
-    pdb.gimp_message("actual hardness: {a}".format(a=actual_hardness)) 
-    
+     
     actual_shape = pdb.gimp_brush_set_shape(actual_name, 1) # enum for GIMP_BRUSH_GENERATED_SQUARE doesn't work?
     pdb.gimp_message("actual shape: {a}".format(a=actual_shape)) 
     
-    actual_radius = pdb.gimp_brush_set_radius(actual_name, 0.5)
-    pdb.gimp_message("actual radius: {a}".format(a=actual_radius)) 
-    
+    pdb.gimp_context_set_brush_size(1)  
     pdb.gimp_context_set_brush(actual_name)
 
     draw = pdb.gimp_image_active_drawable(img)
     
-    points = [10, 10]
-    pdb.gimp_pencilbrush_default(draw, len(points), points)
+    pdb.gimp_context_set_background((255, 255, 255, 255))
 
-    full_draw = pdb.gimp_image_active_drawable(img)
-
-    pixels = [pdb.gimp_drawable_get_pixel(full_draw, x % 20, x / 20)[1] for x in range(20 * 20)]
-    #pdb.gimp_message("pixels: {a}".format(a=pixels)) 
+    points = [0, 0, 1, 0, 2, 0, 0, 1, 1, 1, 2, 1, 0, 2, 1, 2, 2, 2]
+    pdb.gimp_pencil(draw, len(points), points)
     
-    sections = make_sections(pixels) 
-    #pdb.gimp_message("sections: {a}".format(a=sections)) 
-   
-    section_fill = gimp.Layer(img, "First Section", img.width, img.height, RGB_IMAGE, 100, NORMAL_MODE)
-    section_fill.fill(BACKGROUND_FILL)
-    img.add_layer(section_fill, 1)
-    
-    new_points = [] 
-    for section in sections:
-        for idx in section:
-            new_points += idx_to_ij(20, 20, idx)
+    return [pdb.gimp_drawable_get_pixel(draw, x % img.width, x / img.height)[1] for x in range(img.width * img.height)]
 
-    pdb.gimp_context_set_foreground((255, 0, 0))
 
-    pdb.gimp_pencilbrush_default(section_fill, len(new_points), new_points)
+def do_work():
+    img = gimp.Image(20, 20, RGB)
   
+    pixels = create_pixels(img)
+    pdb.gimp_message("pixels: {a}".format(a=pixels)) 
+  
+    sections = make_sections(pixels, img.width, img.height) 
+    pdb.gimp_message("sections: {a}".format(a=sections)) 
+    
+    section_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 25)]
+    for idx, section in enumerate(sections):
+        section_fill = gimp.Layer(img, "section {n}".format(n=idx), img.width, img.height, RGB_IMAGE, 100, NORMAL_MODE)
+        section_fill.fill(BACKGROUND_FILL)
+        
+        img.add_layer(section_fill, 1)
+        
+        new_points = [] 
+        pdb.gimp_context_set_foreground(section_colors[idx % len(section_colors)])
+        for idx in section:
+            new_points += idx_to_ij(img.width, img.height, idx)
+       
+        if len(new_points) > 0:
+            pdb.gimp_pencil(section_fill, len(new_points), new_points) 
+
 
     # Create a new image window
     gimp.Display(img)
