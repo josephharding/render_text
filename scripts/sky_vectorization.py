99031d832efccb9a752a8f7ce3401f4f6d5f93e0
Joe Harding <joe.harding@us.ibm.com>
2017-06-15 16:58:22 -0500

    first attempt at running script on preexisting image

--- scripts/sky_vectorization.py
+++ scripts/sky_vectorization.py
@@ -45,14 +45,12 @@ def get_pixel_below(grid_width, grid_height, idx):
 
 def average_color(average, color):
     diff = (average[0] - color[0], average[1] - color[1], average[2] - color[2])
-    #pdb.gimp_message("color diff: {d}".format(d=diff))
     return (average[0] + diff[0], average[1] + diff[1], average[2] + diff[2])
 
 
 def is_color_close(average, color):
-    diff = (average[0] - color[0]) + (average[1] - color[1]) + (average[2] - color[2])
-    #pdb.gimp_message("color diff: {d}".format(d=diff))
-    return diff < 5
+    _e = 3
+    return abs(average[0] - color[0]) < _e and abs(average[1] - color[1]) < _e and abs(average[2] - color[2]) < _e
 
 
 # returns an array of pixel indices that belong to the current section
@@ -61,7 +59,7 @@ def scan_pixels(current_index, pixels, pixel_sectioned, color_average, width, he
     result = []
     if current_index is not None and current_index not in pixel_sectioned:
         if is_color_close(color_average, pixels[current_index]):
-            color_average = average_color(color_average, pixels[current_index]) 
+            #color_average = average_color(color_average, pixels[current_index]) 
             
             result.append(current_index) 
             pixel_sectioned[current_index] = True 
@@ -70,6 +68,8 @@ def scan_pixels(current_index, pixels, pixel_sectioned, color_average, width, he
             result += scan_pixels(get_pixel_left(width, height, current_index), pixels, pixel_sectioned, color_average, width, height)
             result += scan_pixels(get_pixel_above(width, height, current_index), pixels, pixel_sectioned, color_average, width, height)
             result += scan_pixels(get_pixel_below(width, height, current_index), pixels, pixel_sectioned, color_average, width, height)
+        else:
+            pdb.gimp_message("index: {i} failed color test".format(i=current_index))
 
     return result
 
@@ -89,6 +89,40 @@ def make_sections(pixels, width, height):
             sections.append(new_section)
 
     return sections
+
+
+def vectorize(pixels, img):
+    sections = make_sections(pixels, img.width, img.height) 
+    pdb.gimp_message("sections: {a}".format(a=sections)) 
+    
+    actual_name = pdb.gimp_brush_new('joe')
+    pdb.gimp_message("brush name: {a}".format(a=actual_name)) 
+     
+    actual_shape = pdb.gimp_brush_set_shape(actual_name, 1) # enum for GIMP_BRUSH_GENERATED_SQUARE doesn't work?
+    pdb.gimp_message("actual shape: {a}".format(a=actual_shape)) 
+    
+    pdb.gimp_context_set_brush_size(1)  
+    pdb.gimp_context_set_brush(actual_name)
+    
+    section_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    for idx, section in enumerate(sections):
+        pdb.gimp_context_set_foreground(section_colors[idx % len(section_colors)])
+        
+        section_fill = gimp.Layer(img, "section {n}".format(n=idx), img.width, img.height, RGB_IMAGE, 100, NORMAL_MODE)
+        section_fill.fill(BACKGROUND_FILL) 
+        img.add_layer(section_fill, idx + 1)
+        
+        new_points = [] 
+        for idx in section:
+            new_points += idx_to_ij(img.width, img.height, idx)
+      
+        pdb.gimp_message("new points: {a}".format(a=new_points))
+        if len(new_points) > 0:
+            pdb.gimp_pencil(section_fill, len(new_points), new_points) 
+
+
+def get_pixels(img, draw):
+    return [pdb.gimp_drawable_get_pixel(draw, x % img.width, x / img.height)[1] for x in range(img.width * img.height)]
 
 
 def create_pixels(img):
@@ -116,35 +150,19 @@ def create_pixels(img):
 
     points = [0, 0, 1, 0, 2, 0, 0, 1, 1, 1, 2, 1, 0, 2, 1, 2, 2, 2]
     pdb.gimp_pencil(draw, len(points), points)
-    
-    return [pdb.gimp_drawable_get_pixel(draw, x % img.width, x / img.height)[1] for x in range(img.width * img.height)]
+
+    return get_pixels(img, draw)
 
 
-def do_work():
+def make_image():
+    pdb.gimp_message("making image...")
     img = gimp.Image(20, 20, RGB)
-  
+
     pixels = create_pixels(img)
     pdb.gimp_message("pixels: {a}".format(a=pixels)) 
-  
-    sections = make_sections(pixels, img.width, img.height) 
-    pdb.gimp_message("sections: {a}".format(a=sections)) 
     
-    section_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 25)]
-    for idx, section in enumerate(sections):
-        section_fill = gimp.Layer(img, "section {n}".format(n=idx), img.width, img.height, RGB_IMAGE, 100, NORMAL_MODE)
-        section_fill.fill(BACKGROUND_FILL)
-        
-        img.add_layer(section_fill, 1)
-        
-        new_points = [] 
-        pdb.gimp_context_set_foreground(section_colors[idx % len(section_colors)])
-        for idx in section:
-            new_points += idx_to_ij(img.width, img.height, idx)
-       
-        if len(new_points) > 0:
-            pdb.gimp_pencil(section_fill, len(new_points), new_points) 
-
-
+    vectorize(pixels, img) 
+    
     # Create a new image window
     gimp.Display(img)
     # Show the new image window
@@ -153,17 +171,40 @@ def do_work():
     pdb.gimp_context_pop()
 
 
+def process_image(img, drw):
+    pdb.gimp_message("processing image...")
+    
+    pixels = get_pixels(img, drw)
+    pdb.gimp_message("pixels: {a}".format(a=pixels)) 
+    
+    vectorize(pixels, img)
+
+
 register(
     "python_fu_vectorize_sky",
-    "vectorize sky",
-    "vectorize a raster",
+    "Vectorize Sky",
+    "Vectorize Sky",
     "Joe Harding",
     "Joe Harding",
     "2017",
-    "Vectorize Sky",
-    "",      # Create a new image, don't work on an existing one
+    "<Image>/Image/Vectorize Sky",
+    "",
     [],
     [],
-    do_work, menu="<Image>/Filters")
+    process_image, domain=("gimp20-python", gimp.locale_directory))
+
+
+#register(
+#    "python_fu_vectorize_sky",
+#    "vectorize sky",
+#    "vectorize a raster",
+#    "Joe Harding",
+#    "Joe Harding",
+#    "2017",
+#    "Vectorize Sky",
+#    "",
+#    [],
+#    [],
+#    make_image, menu="<Image>/Filters")
 
 main()
