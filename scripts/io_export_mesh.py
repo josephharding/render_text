5256eaa8a446170a836e67fa5475ff5cc7bc0c89
Joe Harding <joe.harding@us.ibm.com>
2018-07-17 14:36:48 -0500

    well, once again, found this uncommitted from who knows what knows what..

--- scripts/io_export_mesh.py
+++ scripts/io_export_mesh.py
@@ -1,6 +1,6 @@
 
 bl_info = {
-    "name": "Joe Mesh Export",
+    "name": "WebGL Mesh Export",
     "description": "mesh exporter",
     "author": "Joe Harding",
     "version": (1, 0),
@@ -21,7 +21,7 @@ import mathutils
 from bpy_extras.io_utils import ExportHelper
 
 
-class JoeMesh(bpy.types.Operator, ExportHelper):
+class WebGLMesh(bpy.types.Operator, ExportHelper):
 
     filename_ext = ".json"
 
@@ -38,13 +38,27 @@ class JoeMesh(bpy.types.Operator, ExportHelper):
     def execute(self, context):
         print("### Mesh Export Script Start ###")
 
+        if not 'working_scene' in bpy.data.scenes:
+            print("ERROR: you must have scene 'working_scene' set in your project")
+            return {'CANCELLED'}
+
+        # remove old export
+        self.clear_old_export()
+
+        # clone working scene into export scene
+        self.clone_working_scene()
+
         bones = []
         indices = []
         verts = []
         normals = []
         uvs = []
-        
-        for obj in bpy.data.objects: 
+      
+        bpy.ops.object.select_all(action='DESELECT')
+       
+        print("scene I'm in:", bpy.context.screen.scene) 
+        for obj in bpy.context.scene.objects.data.objects: 
+            print("info: exporting object:", obj.name)
             if obj.type == 'ARMATURE':
                 for bone in obj.data.bones: 
                     bones.append({
@@ -54,7 +68,12 @@ class JoeMesh(bpy.types.Operator, ExportHelper):
                         }) 
 
             else:
+                obj.select = True
+                bpy.context.scene.objects.active = obj
+                
+                print("****MODE A:", bpy.context.mode) 
                 bpy.ops.object.mode_set(mode='EDIT', toggle=False) 
+                print("****MODE B:", bpy.context.mode) 
                 bm = bmesh.from_edit_mesh(obj.data)
                 bmesh.ops.triangulate(bm, faces=bm.faces[:], quad_method=0, ngon_method=0)
                 uv_layer = bm.loops.layers.uv.active
@@ -134,6 +153,85 @@ class JoeMesh(bpy.types.Operator, ExportHelper):
         file.close()
 
 
+    def clone_working_scene(self):
+        # switch back to working scene¬
+        bpy.context.screen.scene = bpy.data.scenes['working_scene']
+        bpy.context.scene.update()
+        
+        copy_list = []
+        for obj in bpy.context.scene.objects:
+            if obj.type == 'MESH':
+                data_copy = obj.data.copy()
+                obj_copy = obj.copy()
+                obj_copy.data = data_copy
+                copy_list.append(obj_copy)
+
+        print("copying objects from current scene...")
+        print(copy_list)
+
+        bpy.ops.scene.new()
+        bpy.context.scene.name = 'export_scene'
+
+        for copy in copy_list:
+            bpy.data.scenes['export_scene'].objects.link(copy)
+
+        bpy.context.screen.scene = bpy.data.scenes['working_scene']
+        bpy.context.scene.update()
+
+        # join all the objects in the export scene
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.objects.active = None
+
+        bpy.context.screen.scene = bpy.data.scenes['export_scene']
+        bpy.context.scene.update()
+
+        for obj in bpy.data.scenes['export_scene'].objects:
+            print("selecting object to join...")
+            print(obj)
+            obj.data.name = "new"
+            obj.select = True
+            bpy.context.scene.objects.active = obj
+
+        bpy.ops.object.join()
+
+        # setting the name of an object
+        bpy.context.object.name = 'export_object'
+
+        # setting the name of the data of an object
+        bpy.context.object.data.name = 'export_data'
+
+
+    def clear_old_export(self):
+        # start off by deselecting everything
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.scene.objects.active = None
+
+        # clean up any already existing export scenes
+        if 'export_scene' in bpy.data.scenes:
+            for obj in bpy.data.scenes['export_scene'].objects:
+                obj.select = True
+
+            bpy.ops.object.delete()
+
+            print("removing already existing 'export_scene'...")
+            bpy.context.screen.scene = bpy.data.scenes['export_scene']
+            bpy.ops.scene.delete()
+
+        if 'export_object' in bpy.data.objects:
+            print("cleaning up already existing 'export_object' object")
+            bpy.data.objects['export_object'].data = None
+            bpy.data.objects.remove(bpy.data.objects['export_object'])
+
+        if 'export_data' in bpy.data.meshes:
+            print("cleaning up already existing 'export_data' mesh")
+            if bpy.data.meshes['export_data'].users != 0:
+                print("ERROR: previous export data still exists")
+            else:
+                bpy.data.meshes.remove(bpy.data.meshes['export_data'])
+
+
+
     def invoke(self, context, event):
         wm = context.window_manager
 
@@ -153,16 +251,16 @@ class JoeMesh(bpy.types.Operator, ExportHelper):
 
 
 def menu_func_export(self, context):
-    self.layout.operator(JoeMesh.bl_idname, text="Joe Mesh")
+    self.layout.operator(WebGLMesh.bl_idname, text="Joe Mesh")
 
 
 def register():
-    bpy.utils.register_class(JoeMesh)
+    bpy.utils.register_class(WebGLMesh)
     bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_class(JoeMesh)
+    bpy.utils.unregister_class(WebGLMesh)
     bpy.types.INFO_MT_file_export.remove(menu_func_export)
 
 
